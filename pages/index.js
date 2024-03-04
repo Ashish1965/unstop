bd376ec20b72c066155f012506511a171a3f0b00
Ashish1965 <121724473+Ashish1965@users.noreply.github.com>
2024-03-04 19:07:38 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
   }
 
   return (
-    <div>
+    <div className="w-full">
       <section className="text-gray-600 body-font relative">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col text-center w-full mb-12">
